2f6a9251c50e820ee9daba0eb917efae04713d6b
connorjnel <connor.j.nel00@gmail.com>
2022-10-06 19:35:43 +0100

    style tweaks

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Scroll = (props) => {
-	return <div style={{ overflowY: "scroll", border: "2px solid #333", height: "80vh" }}>{props.children}</div>;
+	return <div style={{ overflowY: "scroll", border: "2px solid #333", height: "90vh" }}>{props.children}</div>;
 };
 
 export default Scroll;
